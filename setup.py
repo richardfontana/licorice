3bc71a8d46b1a62df07c5f935d00de02de614ad3
Richard Fontana <fontana@sharpeleven.org>
2016-04-26 16:30:45 -0400

    Correct license classifier in setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
         ]
